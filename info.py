99449004e70a153c84cedc1a05df220938d11093
harshithacharya632-source <harshithacharya632@gmail.com>
2025-09-16 11:37:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -7,7 +7,7 @@ id_pattern = re.compile(r'^.\d+$')
 SESSION = environ.get('SESSION', 'TechVJBot')
 API_ID = int(environ.get('API_ID', '25962591'))
 API_HASH = environ.get('API_HASH', '5d1dc00807bc62b346488a483bd3075d')
-BOT_TOKEN = environ.get('BOT_TOKEN', "7011228023:AAG-cnuOaMMANjuV_hbDv3Ebfexd2RmJEFc")
+BOT_TOKEN = environ.get('BOT_TOKEN', "7011228023:AAEkyjKftHr0XieSZbVSKF9qrsVQ4yi99Qg")
 
 # Bot settings
 PORT = environ.get("PORT", "8080")
