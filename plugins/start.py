686ecce1bc8791cd4546498cba1f4944842162ed
harshithacharya632-source <harshithacharya632@gmail.com>
2025-09-17 16:37:29 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -106,25 +106,25 @@ from utils import temp, get_shortlink
 
 
 @Client.on_message(filters.command("start") & filters.incoming)
-# async def start(client, message):
-#     if not await db.is_user_exist(message.from_user.id):
-#         await db.add_user(message.from_user.id, message.from_user.first_name)
-#         await client.send_message(
-#             LOG_CHANNEL,
-#             script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention)
-#         )
-#     rm = InlineKeyboardMarkup(
-#         [[
-#             InlineKeyboardButton("✨ Update Channel", url="https://t.me/trendi_Backup")
-#         ]]
-#     )
-#     await client.send_message(
-#         chat_id=message.from_user.id,
-#         text=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
-#         reply_markup=rm,
-#         parse_mode=enums.ParseMode.HTML
-#     )
-#     return
+ async def start(client, message):
+     if not await db.is_user_exist(message.from_user.id):
+         await db.add_user(message.from_user.id, message.from_user.first_name)
+         await client.send_message(
+             LOG_CHANNEL,
+             script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention)
+         )
+     rm = InlineKeyboardMarkup(
+         [[
+             InlineKeyboardButton("✨ Update Channel", url="https://t.me/trendi_Backup")
+         ]]
+     )
+     await client.send_message(
+         chat_id=message.from_user.id,
+         text=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+         reply_markup=rm,
+         parse_mode=enums.ParseMode.HTML
+     )
+     return
 
 
 @Client.on_message(filters.private & (filters.document | filters.video))
