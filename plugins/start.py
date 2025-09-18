9326dfef8d081850b46fdb857f0d9ee2bebb8247
harshithacharya632-source <harshithacharya632@gmail.com>
2025-09-18 20:01:58 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,131 +1,6 @@
-# import random
-# import humanize
-# from Script import script
-# from pyrogram import Client, filters, enums
-# from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply, CallbackQuery
-# from info import URL, LOG_CHANNEL, SHORTLINK
-# from urllib.parse import quote_plus
-# from TechVJ.util.file_properties import get_name, get_hash, get_media_file_size
-# from TechVJ.util.human_readable import humanbytes
-# from database.users_chats_db import db
-# from utils import temp, get_shortlink
-
-
-# @Client.on_message(filters.command("start") & filters.incoming)
-# async def start(client, message):
-#     if not await db.is_user_exist(message.from_user.id):
-#         await db.add_user(message.from_user.id, message.from_user.first_name)
-#         await client.send_message(
-#             LOG_CHANNEL,
-#             script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention)
-#         )
-#     rm = InlineKeyboardMarkup(
-#         [[
-#             InlineKeyboardButton("✨ Update Channel", url="https://t.me/trendi_Backup")
-#         ]]
-#     )
-#     await client.send_message(
-#         chat_id=message.from_user.id,
-#         text=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
-#         reply_markup=rm,
-#         parse_mode=enums.ParseMode.HTML
-#     )
-#     return
-
-
-# @Client.on_message(filters.private & (filters.document | filters.video))
-# async def stream_start(client, message):
-#     file = getattr(message, message.media.value)
-#     filename = file.file_name
-#     filesize = humanize.naturalsize(file.file_size)
-#     fileid = file.file_id
-#     user_id = message.from_user.id
-#     username = message.from_user.mention
-
-#     # Forward file to LOG_CHANNEL
-#     log_msg = await client.send_cached_media(
-#         chat_id=LOG_CHANNEL,
-#         file_id=fileid,
-#     )
-
-#     fileName = {quote_plus(get_name(log_msg))}
-
-#     # Generate stream & download links
-#     if SHORTLINK is False:
-#         stream = f"{URL}/watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
-#         download = f"{URL}/download/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
-#     else:
-#         stream = await get_shortlink(
-#             f"{URL}/watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
-#         )
-#         download = await get_shortlink(
-#             f"{URL}/download/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
-#         )
-
-#     # Log message
-#     await log_msg.reply_text(
-#         text=f"•• ʟɪɴᴋ ɢᴇɴᴇʀᴀᴛᴇᴅ ꜰᴏʀ ɪᴅ #{user_id} \n•• ᴜꜱᴇʀɴᴀᴍᴇ : {username} \n\n•• ᖴᎥᒪᗴ Nᗩᗰᴇ : {fileName}",
-#         quote=True,
-#         disable_web_page_preview=True,
-#         reply_markup=InlineKeyboardMarkup(
-#             [[
-#                 InlineKeyboardButton("🚀 Fast Download 🚀", url=download),
-#                 InlineKeyboardButton("🖥 Watch online 🖥", url=stream)
-#             ]]
-#         )
-#     )
-
-#     # Reply to user
-#     rm = InlineKeyboardMarkup(
-#         [
-#             [
-#                 InlineKeyboardButton("sᴛʀᴇᴀᴍ 🖥", url=stream),
-#                 InlineKeyboardButton("ᴅᴏᴡɴʟᴏᴀᴅ 📥", url=download)
-#             ]
-#         ]
-#     )
-#     msg_text = """<i><u>𝗬𝗼𝘂𝗿 𝗟𝗶𝗻𝗸 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲𝗱 !</u></i>\n\n<b>📂 Fɪʟᴇ ɴᴀᴍᴇ :</b> <i>{}</i>\n\n<b>📦 Fɪʟᴇ ꜱɪᴢᴇ :</b> <i>{}</i>\n\n<b>📥 Dᴏᴡɴʟᴏᴀᴅ :</b> <i>{}</i>\n\n<b>🖥 Wᴀᴛᴄʜ :</b> <i>{}</i>\n\n<b>🚸 Nᴏᴛᴇ : ʟɪɴᴋ ᴡᴏɴ'ᴛ ᴇxᴘɪʀᴇ ᴛɪʟʟ ɪ ᴅᴇʟᴇᴛᴇ</b>"""
-
-#     await message.reply_text(
-#         text=msg_text.format(get_name(log_msg), humanbytes(get_media_file_size(message)), download, stream),
-#         quote=True,
-#         disable_web_page_preview=True,
-#         reply_markup=rm
-#     )
-import random
-import humanize
-from Script import script
-from pyrogram import Client, filters, enums
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply, CallbackQuery
-from info import URL, LOG_CHANNEL, SHORTLINK
-from urllib.parse import quote_plus
-from TechVJ.util.file_properties import get_name, get_hash, get_media_file_size
-from TechVJ.util.human_readable import humanbytes
-from database.users_chats_db import db
-from utils import temp, get_shortlink
-
-
-@Client.on_message(filters.command("start") & filters.incoming)
-async def start(client, message):
-     if not await db.is_user_exist(message.from_user.id):
-         await db.add_user(message.from_user.id, message.from_user.first_name)
-         await client.send_message(
-             LOG_CHANNEL,
-             script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention)
-         )
-     rm = InlineKeyboardMarkup(
-         [[
-             InlineKeyboardButton("✨ Update Channel", url="https://t.me/trendi_Backup")
-         ]]
-     )
-     await client.send_message(
-         chat_id=message.from_user.id,
-         text=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
-         reply_markup=rm,
-         parse_mode=enums.ParseMode.HTML
-     )
-     return
-
+import os
+from pathlib import Path
+from ffmpeg_utils import ensure_multi_audio_mp4
 
 @Client.on_message(filters.private & (filters.document | filters.video))
 async def stream_start(client, message):
@@ -136,7 +11,17 @@ async def stream_start(client, message):
     user_id = message.from_user.id
     username = message.from_user.mention
 
-    # Forward file to LOG_CHANNEL
+    # Download file locally first
+    download_dir = Path("downloads")
+    download_dir.mkdir(exist_ok=True)
+    local_path = await client.download_media(message, file_name=download_dir / filename)
+
+    # Convert to MP4 with all audio tracks (multi-language supported)
+    converted_dir = Path("converted")
+    converted_dir.mkdir(exist_ok=True)
+    converted_path = ensure_multi_audio_mp4(local_path, output_dir=converted_dir)
+
+    # Forward original file to LOG_CHANNEL (optional)
     log_msg = await client.send_cached_media(
         chat_id=LOG_CHANNEL,
         file_id=fileid,
@@ -146,62 +31,37 @@ async def stream_start(client, message):
 
     # Generate stream & download links
     if SHORTLINK is False:
-        stream = f"{URL}/watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
-        download = f"{URL}/download/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
+        stream = f"{URL}/watch/{str(log_msg.id)}/{quote_plus(Path(converted_path).name)}?hash={get_hash(log_msg)}"
+        download = f"{URL}/download/{str(log_msg.id)}/{quote_plus(Path(converted_path).name)}?hash={get_hash(log_msg)}"
     else:
         stream = await get_shortlink(
-            f"{URL}/watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
+            f"{URL}/watch/{str(log_msg.id)}/{quote_plus(Path(converted_path).name)}?hash={get_hash(log_msg)}"
         )
         download = await get_shortlink(
-            f"{URL}/download/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
+            f"{URL}/download/{str(log_msg.id)}/{quote_plus(Path(converted_path).name)}?hash={get_hash(log_msg)}"
         )
 
-    # Log message
-    await log_msg.reply_text(
-        text=f"•• ʟɪɴᴋ ɢᴇɴᴇʀᴀᴛᴇᴅ ꜰᴏʀ ɪᴅ #{user_id} \n•• ᴜꜱᴇʀɴᴀᴍᴇ : {username} \n\n•• ᖴᎥᒪᗴ Nᗩᗰᴇ : {fileName}",
-        quote=True,
-        disable_web_page_preview=True,
-        reply_markup=InlineKeyboardMarkup(
-            [[
-                InlineKeyboardButton("🚀 Fast Download 🚀", url=download),
-                InlineKeyboardButton("🖥 Watch online 🖥", url=stream)
-            ]]
-        )
-    )
+    # Reply to user
+    rm = InlineKeyboardMarkup([
+        [InlineKeyboardButton("sᴛʀᴇᴀᴍ 🖥", url=stream),
+         InlineKeyboardButton("ᴅᴏᴡɴʟᴏᴀᴅ 📥", url=download)]
+    ])
 
-    # Buttons for user
-    rm = InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton("sᴛʀᴇᴀᴍ 🖥", url=stream),
-                InlineKeyboardButton("ᴅᴏᴡɴʟᴏᴀᴅ 📥", url=download)
-            ]
-        ]
-    )
-
-    # Message text (no raw links)
-    msg_text = """<i><u>𝗬𝗼𝘂𝗿 𝗟𝗶𝗻𝗸 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲𝗱 !</u></i>\n\n
+    msg_text = """<i><u>𝗬𝗼𝘂𝗿 𝗟𝗶𝗻𝗸 𝗚𝗲𝗻𝗲ʀ𝗮𝘁𝗲𝗱 !</u></i>\n\n
 <b>📂 Fɪʟᴇ ɴᴀᴍᴇ :</b> <i>{}</i>\n\n
 <b>📦 Fɪʟᴇ ꜱɪᴢᴇ :</b> <i>{}</i>\n\n
 <b>🚸 Nᴏᴛᴇ : ʟɪɴᴋ ᴡᴏɴ'ᴛ ᴇxᴘɪʀᴇ 24 HOURS</b>"""
 
     await message.reply_text(
         text=msg_text.format(
-            get_name(log_msg),                       # filename
-            humanbytes(get_media_file_size(message)),# filesize
-            download,                                # passed but unused
-            stream                                   # passed but unused
+            Path(converted_path).name,
+            humanbytes(get_media_file_size(message)),
         ),
         quote=True,
         disable_web_page_preview=True,
-        reply_markup=rm
+        reply_markup=rm,
+        parse_mode=enums.ParseMode.HTML
     )
 
-    # # Optional: auto-delete warning like your screenshot
-    # warn_msg = await message.reply_text(
-    #     "❗❗❗ <b>IMPORTANT</b> ❗❗❗\n\n"
-    #     "This message will be <b>deleted in 10 mins</b> (due to copyright issues).\n\n"
-    #     "👉 Please forward this message to your <b>Saved Messages</b> or any <b>private chat</b>.",
-    #     parse_mode=enums.ParseMode.HTML
-    # )
-    # await warn_msg.delete(delay=600)  # auto delete after 600 sec (10 mins)
+    # Optional: cleanup old downloads to save space
+    os.remove(local_path)
