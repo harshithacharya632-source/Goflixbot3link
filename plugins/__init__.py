8bfda27c676dcf9d5f874755cf63c0cf11c5b0ba
harshithacharya632-source <harshithacharya632@gmail.com>
2025-09-20 13:23:32 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,9 +1,14 @@
-# utils/__init__.py
+# Don't Remove Credit @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
+# Ask Doubt on telegram @KingVJ01
 
-# This file can be empty, or just contain package-level docstring
-"""
-Utils package for Goflixbot
-Contains helper modules like:
-- utils.py
-- ffmpeg_utils.py
-"""
+from aiohttp import web
+from .route import routes
+
+async def web_server():
+    """
+    Create and return the aiohttp web application.
+    """
+    web_app = web.Application(client_max_size=30000000)
+    web_app.add_routes(routes)
+    return web_app
